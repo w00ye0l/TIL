62f4fafdd98b71fb6e6769cc6943dbefc18b05ac
w00ye0l <dnduf158@gmail.com>
2022-08-09 10:10:16 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/2615.py
+++ Baekjoon/Python/2615.py
@@ -1,39 +1,44 @@
-def game():
-    for i in range(19):
-        for j in range(19):
-            if board[i][j]:
-                for k in range(4):
-                    ni = i + di[k]
-                    nj = j + dj[k]
-                    cnt = 1
+dy = [0, 1, -1, 1]
+dx = [1, 0, 1, 1]
+N = 19
 
-                    while 0 <= ni < 19 and 0 <= nj < 19 and board[ni][nj] == board[i][j]:
-                        cnt += 1
+board = []
 
-                        if cnt == 5:
-                            if 0 <= ni + di[k] < 19 and 0 <= nj + dj[k] < 19:
-                                if board[ni + di[k]][nj + dj[k]] == board[ni][nj]:
-                                    break
+for _ in range(N):
+    temp_list = list(map(int, input().split()))
+    board.append(temp_list)
 
-                            if 0 <= i - di[k] < 19 and 0 <= j - dj[k] < 19:
-                                if board[i - di[k]][j - dj[k]] == board[i][j]:
-                                    break
-                            return board[i][j], i+1, j+1
+answer = 0
 
-                        ni += di[k]
-                        nj += dj[k]
+for y in range(N):
+    for x in range(N):
+        if board[y][x]:
+            for d in range(4):
+                stone_count = 1
 
-    return 0, -1, -1
+                ny = y + dy[d]
+                nx = x + dx[d]
 
+                while True:
+                    if not (-1 < ny < N and -1 < nx < N):
+                        break
 
-di = [1, 1, 0, -1]
-dj = [0, 1, 1, 1]
+                    if board[ny][nx] != board[y][x]:
+                        break
 
-board = [list(map(int, input().split())) for _ in range(19)]
+                    stone_count += 1
 
-winner, x, y = game()
-if not winner:
-    print(winner)
-else:
-    print(winner)
-    print(x, y)
+                    ny += dy[d]
+                    nx += dx[d]
+
+                if stone_count == 5:
+                    prev_y = y - dy[d]
+                    prev_x = x - dx[d]
+
+                    if not (-1 < prev_y < N and -1 < prev_x < N) or board[y][x] != board[prev_y][prev_x]:
+                        print(board[y][x])
+                        print(y + 1, x + 1)
+                        answer = board[y][x]
+
+if answer == 0:
+    print(answer)
