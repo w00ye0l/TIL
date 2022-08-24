c03142f2e0a8ed45fa8ae60dbce02ccc47f4115b
w00ye0l <dnduf158@gmail.com>
2022-08-25 00:42:21 +0900

    ADD Baekjoon solved with Python

--- Baekjoon/Python/1769.py
+++ Baekjoon/Python/1769.py
@@ -1,13 +1,21 @@
-x = list(map(int, input()))
-
 cnt = 0
 
-while len(x) != 1:
-    sum_ = sum(x)
-    x = list(map(int, str(sum_)))
-    cnt += 1
 
-print(cnt)
+def three(x):
+    global cnt
+
+    if len(x) != 1:
+        cnt += 1
+        x = list(map(int, str(sum(x))))
+        return three(x)
+    else:
+        return cnt
+
+
+x = list(map(int, input()))
+
+print(three(x))
+
 if sum(x) % 3 == 0:
     print('YES')
 else:
