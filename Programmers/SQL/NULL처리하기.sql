8e5d0d6f836d7a4e607242ade66ec3be0170344e
w00ye0l <dnduf158@gmail.com>
2022-08-26 17:25:17 +0900

    ADD Programmers solved with SQL

---
+++ Programmers/SQL/NULL처리하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT 
+    ANIMAL_TYPE,
+    CASE NAME 
+        WHEN NAME THEN NAME
+        ELSE "No name"
+    END AS NAME, 
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
