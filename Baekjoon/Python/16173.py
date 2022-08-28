bd1b779d107da6d1d11a08bbc96f924e72aa0c06
w00ye0l <dnduf158@gmail.com>
2022-08-28 22:29:49 +0900

    ADD Baekjoon solved with Python

---
+++ Baekjoon/Python/16173.py
@@ -0,0 +1,39 @@
+dy = [0, 1]
+dx = [1, 0]
+
+
+def dfs():
+    stack = []
+    stack.append((0, 0))
+
+    while stack:
+        (y, x) = stack.pop()
+        cnt = 1
+        visited[y][x] = cnt
+
+        for i in range(2):
+            ny = y + (dy[i] * board[y][x])
+            nx = x + (dx[i] * board[y][x])
+
+            if ny < n and nx < n:
+                if not visited[ny][nx]:
+                    cnt += 1
+                    visited[ny][nx] = cnt
+                    stack.append((ny, nx))
+
+    return
+
+
+n = int(input())
+
+board = []
+visited = [[0] * n for _ in range(n)]
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+dfs()
+
+if visited[n - 1][n - 1]:
+    print("HaruHaru")
+else:
+    print("Hing")
