a899b0b1d1836b84cf497a78dc3bf8e4b7e433ec
w00ye0l <dnduf158@gmail.com>
2022-09-30 00:22:56 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/23253.py
+++ Baekjoon/Python/23253.py
@@ -9,8 +9,6 @@ for i in range(m):
     ki = int(input())
     stack_ = list(map(int, input().split()))
 
-    print(stack_)
-
     comparison = stack_.pop()
 
     while len(stack_) != 0:
@@ -19,9 +17,5 @@ for i in range(m):
         else:
             sign = 'No'
             break
-
-    # for j in range(ki - 1):
-    #     if stack_[j] < stack_[j + 1]:
-    #         sign = 'No'
 
 print(sign)
