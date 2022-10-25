d41608e9791c0606b486128a80ebde63553777cf
w00ye0l <dnduf158@gmail.com>
2022-10-26 01:46:52 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/9251.py
+++ Baekjoon/Python/9251.py
@@ -9,12 +9,13 @@ for i in range(len1):
     cnt = 0
     for j in range(len2):
         # 현재까지 공통부분 중 가장 긴 것 찾기
+        # print(cnt, j, dp[j])
         if cnt < dp[j]:
             cnt = dp[j]
         # 공통부분이 더 있을 경우 최대 길이 늘리기
         elif str1[i] == str2[j]:
             dp[j] = cnt + 1
-        print(str1[i], str2[j], cnt)
-        print(dp)
+            # print(str1[i], str2[j], cnt)
+            # print(dp)
 
 print(max(dp))
