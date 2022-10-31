ab23a71243be2bb3fff687a7512a8ae488b9a6b6
w00ye0l <dnduf158@gmail.com>
2022-10-31 22:38:00 +0900

    ADD Baekjoon solved with Python

--- Baekjoon/Python/11052.py
+++ Baekjoon/Python/11052.py
@@ -2,18 +2,10 @@ n = int(input())
 
 cost = list(map(int, input().split()))
 
-dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+dp = [0 for _ in range(n + 1)]
 
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        print(j, i)
-        if j % i == 0:
-            dp[i][j] = cost[i - 1] * (j // i)
-        print(dp)
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + cost[j - 1])
 
-max_ = 0
-for i in range(1, n + 1):
-    if max_ < dp[i][-1]:
-        max_ = dp[i][-1]
-
-print(max_)
+print(dp[-1])
