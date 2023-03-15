1a39a9686debea16740753087789fd1d65e3afa3
wooyeol <dnduf158@gmail.com>
2023-03-15 22:15:44 +0900

    ADD Baekjoon solved with Python

---
+++ Baekjoon/Python/10991.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    if i != n:
+        print()
