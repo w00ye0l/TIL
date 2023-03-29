681b1cb62bd806eb756001b333beeea01503e977
wooyeol <dnduf158@gmail.com>
2023-03-29 23:20:54 +0900

    ADD Baekjoon solved with Python

---
+++ Baekjoon/Python/27889.py
@@ -0,0 +1,8 @@
+school = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy",
+}
+
+print(school[input()])
