7d48e4c3c910135e34d1e9d6296d20bc8ee09459
wooyeol <dnduf158@gmail.com>
2023-04-09 23:30:36 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/1629.py
+++ Baekjoon/Python/1629.py
@@ -1,3 +1,15 @@
+def solution(a, b, c):
+    if b == 1:
+        return a % c
+
+    temp = solution(a, b // 2, c)
+
+    if b % 2 == 1:
+        return ((temp * temp) % c) * a % c
+    else:
+        return (temp * temp) % c
+
+
 a, b, c = map(int, input().split())
 
-print(pow(a, b, c))
+print(solution(a, b, c))
