7ee028ed2f2bbfd5154abd912da24313a250310e
wooyeol <dnduf158@gmail.com>
2023-04-12 00:45:48 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/1711.py
+++ Baekjoon/Python/1711.py
@@ -6,18 +6,18 @@ input = sys.stdin.readline
 def cal():
     global answer
 
-    n1, n2, n3 = visited[0], visited[1], visited[2]
-    a = (n1[0] - n2[0]) ** 2 + (n1[1] - n2[1]) ** 2
-    b = (n2[0] - n3[0]) ** 2 + (n2[1] - n3[1]) ** 2
-    c = (n3[0] - n1[0]) ** 2 + (n3[1] - n1[1]) ** 2
+    a = (visited[0][0] - visited[1][0]) ** 2 + (visited[0][1] - visited[1][1]) ** 2
+    b = (visited[1][0] - visited[2][0]) ** 2 + (visited[1][1] - visited[2][1]) ** 2
+    c = (visited[2][0] - visited[0][0]) ** 2 + (visited[2][1] - visited[0][1]) ** 2
 
-    if 2 * max(a, b, c) == a + b + c:
+    if a + b == c or a + c == b or b + c == a:
         answer += 1
 
 
 def bt(cnt, start):
     if cnt == 3:
-        return cal()
+        cal()
+        return
 
     for i in range(start, n):
         visited[cnt] = node[i]
