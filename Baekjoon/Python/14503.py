84c24db193752416edc0ea561eb5747ebe26c992
wooyeol <dnduf158@gmail.com>
2023-05-11 16:08:27 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/14503.py
+++ Baekjoon/Python/14503.py
@@ -23,11 +23,11 @@ while True:
         nc = c + dy[d]
 
         if place[nr][nc] == 0 and visited[nr][nc] == 0:
-                visited[nr][nc] = 1
-                r, c = nr, nc
-                cnt += 1
-                wall = 1
-                break
+            visited[nr][nc] = 1
+            r, c = nr, nc
+            cnt += 1
+            wall = 1
+            break
 
     if wall == 0:
         if place[r - dx[d]][c - dy[d]] == 1:
