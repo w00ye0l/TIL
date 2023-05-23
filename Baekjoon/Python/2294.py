d60e82f4f77070a972df833e09bb0d9537300de0
wooyeol <dnduf158@gmail.com>
2023-05-24 01:04:46 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/2294.py
+++ Baekjoon/Python/2294.py
@@ -1,11 +1,15 @@
 n, k = map(int, input().split())
+
 coin_li = sorted([int(input()) for _ in range(n)])
+
 dp = [10001] * (k + 1)
 dp[0] = 0
+
 for i in range(1, k + 1):
     for c in coin_li:
         if i - c < 0:
             break
         else:
             dp[i] = min(dp[i - c] + 1, dp[i])
+
 print(-1 if dp[k] == 10001 else dp[k])
