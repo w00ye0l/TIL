82b06c01ce4b039df5e308bbdf33f8dd59c9aebe
wooyeol <dnduf158@gmail.com>
2023-06-28 13:26:33 +0900

    UPDATE Baekjoon solved with Python

--- Baekjoon/Python/18110.py
+++ Baekjoon/Python/18110.py
@@ -2,6 +2,7 @@ import sys
 
 input = sys.stdin.readline
 
+
 def round_num(n):
     if n - int(n) >= 0.5:
         return int(n) + 1
@@ -25,3 +26,30 @@ else:
         score = score[cut:-cut]
 
     print(round_num(sum(score) / (n - 2 * cut)))
+
+
+# import sys
+# import decimal
+
+# input = sys.stdin.readline
+
+# context = decimal.getcontext()
+
+# context.rounding = decimal.ROUND_HALF_UP
+
+# n = int(input())
+
+# if n == 0:
+#     print(0)
+# else:
+#     score = []
+#     for _ in range(n):
+#         score.append(int(input()))
+
+#     score.sort()
+
+#     cut = int(round(decimal.Decimal(str(n * 0.15)), 0))
+#     if cut != 0:
+#         score = score[cut:-cut]
+
+#     print(round(decimal.Decimal(sum(score) / (n - 2 * cut)), 0))
