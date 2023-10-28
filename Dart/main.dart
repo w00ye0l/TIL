57d5abe57c72f9b0171dddeef7299474b43b8022
wooyeol <dnduf158@gmail.com>
2023-10-29 01:32:55 +0900

    ADD Dart 문서 추가

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,9 +1,35 @@
-typedef UserInfo = Map<String, String>;
-
-String sayHi(UserInfo userInfo) {
-  return "Hi ${userInfo['name']}";
+class Strong {
+  final double strengthLevel = 1500.99;
 }
 
+class QuickRunner {
+  void runQuick() {
+    print("runnnn!");
+  }
+}
+
+class Tall {
+  final double height = 1.99;
+}
+
+enum Team { blue, red }
+
+class Player with Strong, QuickRunner, Tall {
+  final Team team;
+
+  Player({
+    required this.team,
+  });
+}
+
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
 void main() {
-  print(sayHi({"name": 'kim'}));
+  var player = Player(
+    team: Team.red,
+  );
+
+  player.runQuick();
 }
